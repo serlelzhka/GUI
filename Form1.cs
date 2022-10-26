a92481f55391784402c82f103409d3d22debeb6e
serlelzhka <sergeilyashenko333@bk.ru>
2022-10-27 00:49:44 +0800

    button

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic.Logging;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace GUI
@@ -8,6 +9,24 @@ namespace GUI
         {
             InitializeComponent();
             numHappy.Text = Properties.Settings.Default.number.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number;
+
+            try
+            {
+                number = int.Parse(this.numHappy.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Гуляй", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Properties.Settings.Default.number = number;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show(Logic.Compare(number), "Вывод:");
         }
     }
 }
